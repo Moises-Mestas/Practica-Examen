5f9228403b61e76a13979a5d5f460c12a4182145
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-05-04 17:47:14 -0500

    Descripción de los cambios

--- ms_pedido/src/main/java/com/upeu/ms_pedido/repository/PedidoRepository.java
+++ ms_pedido/src/main/java/com/upeu/ms_pedido/repository/PedidoRepository.java
@@ -1,5 +1,6 @@
 package com.upeu.ms_pedido.repository;
 
+import com.upeu.ms_pedido.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PedidoRepository extends JpaRepository<com.upeu.ms_pedido.entity.Pedido, Integer> {
