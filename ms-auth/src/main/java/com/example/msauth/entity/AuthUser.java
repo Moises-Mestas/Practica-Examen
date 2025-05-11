fe4a81a6edc018b6803837393236014c9da86257
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-05-11 11:26:01 -0500

    porque

--- ms-auth/src/main/java/com/example/msauth/entity/AuthUser.java
+++ ms-auth/src/main/java/com/example/msauth/entity/AuthUser.java
@@ -1,5 +1,6 @@
 package com.example.msauth.entity;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,12 +17,9 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthUser {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String userName;
-
     private String password;
 }
